e7eb4c8f913bf2db8d48434644a63abba9f56c2e
HAC22 <deampuleadd@yandex.com>
2024-09-17 19:02:48 +0200

    [FIXES] vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        origin: 'http://localhost:5173',
+        hmr: {
+            host: 'localhost',
+            port: 5173,
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.tsx',
