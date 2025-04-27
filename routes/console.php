b70718b2cf85cfe5cc633343404de8da9b0fa55c
lolmeherti <deampuleadd@yandex.com>
2025-04-27 18:18:22 +0200

    [CHANGES] the refresh job runs on Sunday instead of Monday

--- routes/console.php
+++ routes/console.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('pr:process-next')->everyMinute();
-Schedule::command('pr:refresh-pr-count')->weeklyOn(1, '00:00');
+Schedule::command('pr:refresh-pr-count')->weeklyOn(0, '00:00');
